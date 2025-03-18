f4d896aa8e3166d29f42fa5d2fdad66da74d84fb
Carlomadella <madella871@gmail.com>
2025-03-18 14:03:31 +0100

    [START] creati file necessari compreso il README

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// istruzione 1: inizializzazione variabile contatore
+// istruzione 2: condizione fine ciclo
+// istruzione 3: incremento variabile contatore i = i + 1 => i += 1 => i++ 
+
+// for (let i = 1; i<=10; i++) {
+//     console.log(i);
+// }
